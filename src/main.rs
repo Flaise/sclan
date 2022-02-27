9dbb3546be55e72903e20b1892f927020b708af0
Flaise <x.Flaise@gmail.com>
2022-02-27 12:12:09 -0500

    Moved network related state to a separate module.

--- src/main.rs
+++ src/main.rs
@@ -13,38 +13,34 @@ use tui::{
     Frame, Terminal,
 };
 use unicode_width::UnicodeWidthStr;
-use gethostname::gethostname;
+
+mod network;
+use network::{LANState, Peer};
 
 enum InputMode {
     Normal,
     Editing,
 }
 
-/// App holds the state of the application
 struct App {
-    /// Current value of the input box
     input: String,
-    /// Current input mode
     input_mode: InputMode,
-    /// History of recorded messages
     messages: Vec<String>,
-    name: String,
+    lan: LANState,
 }
 
 impl App {
-    fn new(name: String) -> App {
+    fn new() -> App {
         App {
             input: String::new(),
             input_mode: InputMode::Normal,
             messages: Vec::new(),
-            name,
+            lan: LANState::new(),
         }
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let name = gethostname().into_string().map_err(|_| "Invalid unicode in host name.")?;
-
     // set up terminal
     enable_raw_mode()?;
     let mut stdout = io::stdout();
@@ -52,7 +48,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    let app = App::new(name);
+    let mut app = App::new();
+    app.lan.peers.push(Peer {name: "yeah".into()});
+    app.lan.peers.push(Peer {name: "a".into()});
+    app.lan.peers.push(Peer {name: "b".into()});
+    app.lan.peers.push(Peer {name: "c".into()});
+
     let res = run_app(&mut terminal, app);
 
     // restore terminal
@@ -65,7 +66,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     terminal.show_cursor()?;
 
     if let Err(err) = res {
-        println!("{:?}", err)
+        println!("{:?}", err);
     }
 
     Ok(())
@@ -135,10 +136,10 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
     let cell_messages = top_cells[0];
     let cell_side = top_cells[1];
 
-    let side = Paragraph::new(vec![
+    let mut side_rows = vec![
         Spans::from("computer name:"),
         Spans::from(Span::styled(
-            app.name.clone(),
+            app.lan.local_name.clone(),
             Style::default().add_modifier(Modifier::BOLD)
         )),
         Spans::default(),
@@ -147,32 +148,20 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
             "network:",
             Style::default().add_modifier(Modifier::UNDERLINED)
         )),
-        Spans::from("yeah"),
-        Spans::from("dude's comp"),
-        Spans::from("iFonne"),
-        Spans::from("other"),
-        Spans::default(),
+    ];
 
-        Spans::from(vec![
-            Span::styled("[q]", Style::default().add_modifier(Modifier::BOLD)),
-            Span::raw("=quit "),
-        ]),
-    ]);
+    for peer in app.lan.peers.iter() {
+        side_rows.push(Spans::from(peer.name.clone()));
+    }
+    side_rows.push(Spans::default());
+    side_rows.push(Spans::from(vec![
+        Span::styled("[q]", Style::default().add_modifier(Modifier::BOLD)),
+        Span::raw("=quit "),
+    ]));
+
+    let side = Paragraph::new(side_rows);
     f.render_widget(side, cell_side);
 
-
-    // let messages: Vec<ListItem> = app
-    //     .messages
-    //     .iter()
-    //     .enumerate()
-    //     .map(|(i, m)| {
-    //         let content = vec![Spans::from(Span::raw(format!("{}: {}", i, m)))];
-    //         ListItem::new(content)
-    //     })
-    //     .collect();
-    // let messages =
-    //     List::new(messages).block(Block::default().borders(Borders::ALL).title(""));
-    // f.render_widget(messages, cell_messages);
     let msg = match app.input_mode {
         InputMode::Normal => 
             vec![
@@ -239,61 +228,3 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
         List::new(messages).block(Block::default().borders(Borders::ALL));
     f.render_widget(messages, cell_messages);
 }
-
-
-// use std::net::{UdpSocket, SocketAddr};
-
-// const PORT: u16 = 62634;
-
-// fn main() {
-// let socket = UdpSocket::bind("0.0.0.0:8477")?;
-
-// let send_result = socket.send_to(&message, "255.255.255.255:8477");
-//             if let Err(error) = send_result {
-//                 once! {
-//                     handle(domain, &mut EventError::from_err(error, "sync"));
-//                 }
-
-// }
-
-// //         if error.kind() == ErrorKind::AddrInUse {
-// //             if event.attempts == 4 {
-// //                 warn!("4 attempts and broadcast socket still in use");
-// //             }
-// //         } else {
-// //             once! {
-// //                 warn!("broadcast socket: {}", error);
-// //             }
-// //         }
-// // fn try_init_broadcast(domain: &mut Domain) -> IoResult<()> {
-// //     let socket = UdpSocket::bind("0.0.0.0:8477")?;
-// //     socket.set_broadcast(true)?;
-
-// //     // Timeout is workaround for these issues:
-// //     // https://github.com/rust-lang/rfcs/issues/957
-// //     // https://github.com/rust-lang/rust/issues/23272
-// //     // ^ Can't use shutdown signal to interrupt reading thread.
-// //     socket.set_read_timeout(Some(BROADCAST_INTERVAL))?;
-// //     let socket_b = socket.try_clone()?;
-
-// //     info!("Broadcast ready.");
-
-// //     let mut proceed = ProceedSubject::new();
-// //     let observer = proceed.make_observer();
-
-// //     let trigger = make_entity(domain);
-// //     let id_key = generate_id_key();
-// //     set_data(domain, BroadcastData {
-// //         id_key: id_key.clone(),
-// //         trigger,
-// //         socket: Some(socket_b),
-// //         _proceed: proceed,
-// //     });
-// //     init_timer_imprecise(domain, trigger, BROADCAST_INTERVAL);
-// //     init_uhandler(domain, handle_timer);
-
-// //     let to_domain = remote_signaller_of(domain);
-// //     let source = SourceBroadcast::new(socket, id_key);
-// //     let source = source_proceeds(source, observer);
-// //     let source = show_source_error(source);
-// //     thread_read_signals(to_domain, source, "broadcast".into()).map(|_join_handle| ())
---
+++ src/network.rs
@@ -0,0 +1,83 @@
+use std::net::UdpSocket;
+use gethostname::gethostname;
+
+pub struct LANState {
+    socket: Option<UdpSocket>,
+    pub peers: Vec<Peer>,
+    pub local_name: String,
+}
+
+impl LANState {
+    pub fn new() -> LANState {
+        let name = gethostname().into_string().unwrap_or("???".into());
+
+        LANState {
+            socket: None,
+            peers: vec![],
+            local_name: name,
+        }
+    }
+}
+
+pub struct Peer {
+    pub name: String,
+}
+
+// pub fn ping()
+
+// use std::net::{UdpSocket, SocketAddr};
+
+// const PORT: u16 = 62634;
+
+// fn main() {
+// let socket = UdpSocket::bind("0.0.0.0:8477")?;
+
+// let send_result = socket.send_to(&message, "255.255.255.255:8477");
+//             if let Err(error) = send_result {
+//                 once! {
+//                     handle(domain, &mut EventError::from_err(error, "sync"));
+//                 }
+
+// }
+
+// //         if error.kind() == ErrorKind::AddrInUse {
+// //             if event.attempts == 4 {
+// //                 warn!("4 attempts and broadcast socket still in use");
+// //             }
+// //         } else {
+// //             once! {
+// //                 warn!("broadcast socket: {}", error);
+// //             }
+// //         }
+// // fn try_init_broadcast(domain: &mut Domain) -> IoResult<()> {
+// //     let socket = UdpSocket::bind("0.0.0.0:8477")?;
+// //     socket.set_broadcast(true)?;
+
+// //     // Timeout is workaround for these issues:
+// //     // https://github.com/rust-lang/rfcs/issues/957
+// //     // https://github.com/rust-lang/rust/issues/23272
+// //     // ^ Can't use shutdown signal to interrupt reading thread.
+// //     socket.set_read_timeout(Some(BROADCAST_INTERVAL))?;
+// //     let socket_b = socket.try_clone()?;
+
+// //     info!("Broadcast ready.");
+
+// //     let mut proceed = ProceedSubject::new();
+// //     let observer = proceed.make_observer();
+
+// //     let trigger = make_entity(domain);
+// //     let id_key = generate_id_key();
+// //     set_data(domain, BroadcastData {
+// //         id_key: id_key.clone(),
+// //         trigger,
+// //         socket: Some(socket_b),
+// //         _proceed: proceed,
+// //     });
+// //     init_timer_imprecise(domain, trigger, BROADCAST_INTERVAL);
+// //     init_uhandler(domain, handle_timer);
+
+// //     let to_domain = remote_signaller_of(domain);
+// //     let source = SourceBroadcast::new(socket, id_key);
+// //     let source = source_proceeds(source, observer);
+// //     let source = show_source_error(source);
+// //     thread_read_signals(to_domain, source, "broadcast".into()).map(|_join_handle| ())
