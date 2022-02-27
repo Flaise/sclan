8115fcbc024f253d9bfc6839837c039a50a228b2
Flaise <x.Flaise@gmail.com>
2022-02-27 05:28:55 -0500

    Rearranging instructions and removing unnecessary stuff.

--- src/main.rs
+++ src/main.rs
@@ -69,12 +69,18 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
     loop {
+        if let InputMode::Normal = app.input_mode {
+            // Partial fix for cursor still showing in Cygwin.
+            // Implementation of terminal.draw may need reordered to fully fix it.
+            terminal.hide_cursor()?;
+        }
+
         terminal.draw(|f| ui(f, &app))?;
 
         if let Event::Key(key) = event::read()? {
             match app.input_mode {
                 InputMode::Normal => match key.code {
-                    KeyCode::Char('e') => {
+                    KeyCode::Enter => {
                         app.input_mode = InputMode::Editing;
                     }
                     KeyCode::Char('q') => {
@@ -105,51 +111,77 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
 fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
     let chunks = Layout::default()
         .direction(Direction::Vertical)
-        .margin(2)
+        .margin(1)
         .constraints(
             [
                 Constraint::Min(3),
-                Constraint::Length(1),
                 Constraint::Length(3),
             ]
             .as_ref(),
         )
         .split(f.size());
 
-    let (msg, style) = match app.input_mode {
-        InputMode::Normal => (
+    let cell_input = chunks[1];
+
+    let top_cells = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Min(8),
+            Constraint::Length(16),
+        ].as_ref())
+        .split(chunks[0]);
+
+    let cell_messages = top_cells[0];
+    let cell_instructions = top_cells[1];
+
+    // let messages: Vec<ListItem> = app
+    //     .messages
+    //     .iter()
+    //     .enumerate()
+    //     .map(|(i, m)| {
+    //         let content = vec![Spans::from(Span::raw(format!("{}: {}", i, m)))];
+    //         ListItem::new(content)
+    //     })
+    //     .collect();
+    // let messages =
+    //     List::new(messages).block(Block::default().borders(Borders::ALL).title(""));
+    // f.render_widget(messages, cell_messages);
+    let msg = match app.input_mode {
+        InputMode::Normal => 
             vec![
-                Span::raw("Press "),
-                Span::styled("q", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to exit, "),
-                Span::styled("e", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to start editing."),
+                // Span::raw("Press "),
+                Span::styled("[q]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" = exit . "),
+                Span::styled("[Enter]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" = write"),
             ],
-            Style::default().add_modifier(Modifier::RAPID_BLINK),
-        ),
-        InputMode::Editing => (
+        InputMode::Editing => 
             vec![
-                Span::raw("Press "),
-                Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to stop editing, "),
-                Span::styled("Enter", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to record the message"),
+                // Span::raw("Press "),
+                Span::styled("[Esc]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" = stop editing . "),
+                Span::styled("[Enter]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" = send"),
             ],
-            Style::default(),
-        ),
     };
-    let mut text = Text::from(Spans::from(msg));
-    text.patch_style(style);
+    let text = Text::from(Spans::from(msg));
+    // text.patch_style(style);
     let help_message = Paragraph::new(text);
-    f.render_widget(help_message, chunks[1]);
+    f.render_widget(help_message, cell_instructions);
+
+    // let paragraph = Paragraph::new(text.clone())
+    //     .style(Style::default().bg(Color::White).fg(Color::Black))
+    //     .block(create_block("Left, no wrap"))
+    //     .alignment(Alignment::Left);
+    // .wrap(Wrap { trim: false })
 
     let input = Paragraph::new(app.input.as_ref())
         .style(match app.input_mode {
             InputMode::Normal => Style::default(),
-            InputMode::Editing => Style::default().fg(Color::Yellow),
+            InputMode::Editing => Style::default().fg(Color::Yellow).add_modifier(Modifier::RAPID_BLINK),
         })
-        .block(Block::default().borders(Borders::ALL).title("Input"));
-    f.render_widget(input, chunks[2]);
+        .block(Block::default().borders(Borders::ALL).title(""));
+    f.render_widget(input, cell_input);
     match app.input_mode {
         InputMode::Normal =>
             // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
@@ -159,9 +191,9 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
             // Make the cursor visible and ask tui-rs to put it at the specified coordinates after rendering
             f.set_cursor(
                 // Put cursor past the end of the input text
-                chunks[2].x + app.input.width() as u16 + 1,
+                cell_input.x + app.input.width() as u16 + 1,
                 // Move one line down, from the border to the input line
-                chunks[2].y + 1,
+                cell_input.y + 1,
             )
         }
     }
@@ -176,8 +208,8 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
         })
         .collect();
     let messages =
-        List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
-    f.render_widget(messages, chunks[0]);
+        List::new(messages).block(Block::default().borders(Borders::ALL).title(""));
+    f.render_widget(messages, cell_messages);
 }
 
 
